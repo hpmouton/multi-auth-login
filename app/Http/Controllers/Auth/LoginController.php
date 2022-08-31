6c7ef6868881c48f5f4a4868bcf00e64cc81681b
hpmouton <hubertthedev@gmail.com>
2022-08-31 10:03:53 +0200

    Login function added to login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -37,4 +37,24 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+    public function login(Request $request)
+    {
+        $inputVal = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if(auth()->attempt(array('email' => $inputVal['email'], 'password' => $inputVal['password']))){
+            if (auth()->user()->is_admin == 1) {
+                return redirect()->route('admin.route');
+            }else{
+                return redirect()->route('home');
+            }
+        }else{
+            return redirect()->route('login')
+                ->with('error','Email & Password are incorrect.');
+        }
+    }
 }
